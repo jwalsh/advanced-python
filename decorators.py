b2ae54184c1fefca26bd55b190c467ca2ab55942
Jason Walsh <j@wal.sh>
2024-04-24 16:46:59 -0400

    Ignore the decorator wrapper

--- decorators.py
+++ decorators.py
@@ -1,14 +1,14 @@
 # Decorators Exercise
 
 # Imports 
-
+import time
 
 # Exercise 1: Create a decorator that prints the time taken by a function to execute
 def time_it(func):
-    return Pass
+    pass
 
 # Exercise 2: Create a decorator that prints the arguments passed to a function
 def print_args(func):
-    return Pass
+    pass
 
 # TODO: Complete the exercise
