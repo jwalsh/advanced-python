e1d7848eabfce13a8a466f335dc40f45823063f9
Jason Walsh <j@wal.sh>
2024-07-10 13:12:36 -0400

    `set(nums)`

--- dictionaries_and_sets.py
+++ dictionaries_and_sets.py
@@ -1,3 +1,13 @@
 # Dictionaries and Sets Exercise
 
 # TODO: Complete the exercise
+nums = [100,4,200,1,3,2]
+nums_set = set(nums)
+freq = {}
+for num in nums:
+    if num in freq:
+        freq[num] += 1
+    else:
+        freq[num] = 1
+
+    
